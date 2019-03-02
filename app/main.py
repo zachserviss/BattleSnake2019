1b53440216181f2879a3f80591f609deeda30f7c
Zach Serviss <zacharyserviss@gmail.com>
2019-03-02 10:16:41 -0800

    fixing color/body issue

--- app/main.py
+++ app/main.py
@@ -45,7 +45,7 @@ def start():
     headType = "beluga"
     tailType = "curled"
     
-    return start_response(color,headType,tailType)
+    return start_response(color)
 
 
 @bottle.post('/move')
